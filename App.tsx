57a16df80df3367dcd76fdd275bdbdc02753ab9f
WesleyR10 <wesleyribas2015@gmail.com>
2024-06-07 21:37:12 -0300

    Finish project

--- App.tsx
+++ App.tsx
@@ -1,8 +1,27 @@
 
-import { Groups } from './src/screens/Groups';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from './src/theme';
+
+import { Loading } from '@components/Loading';
+
+import { Routes } from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </>
+    </ThemeProvider>
   );
 }
